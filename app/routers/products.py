3fec3435d2eba6ac7c6f6415a332baaa97433719
mhlohh <rockriderskerala@gmail.com>
2026-07-14 11:40:12 +0530

    fix: remove invalid response_model in products router

--- app/routers/products.py
+++ app/routers/products.py
@@ -6,11 +6,13 @@ from app.database import get_products
 async def all_products():
     return get_products()
 
-router = APIRouter(prefix="/products" ,tags=["products"])
-@router.get("/",response_model = list[Product])
-async def  get_all_products():
+
+router = APIRouter(prefix="/products", tags=["products"])
+
+
+@router.get("/")
+async def get_all_products():
     products = await all_products()
     if not products:
         raise HTTPException(status_code=404, detail="No products found ")
     return products
-    
