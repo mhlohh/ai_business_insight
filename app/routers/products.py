f0524958fc8c25809ca95682ef5081221761949e
mhlohh <rockriderskerala@gmail.com>
2026-07-14 13:44:44 +0530

    feat: add db_router and merge db products API routes into routers/products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,12 +1,31 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas.Database_schema import Product
-from app.database import all_products
+from app.database import all_products, get_product
 
 router = APIRouter(prefix="/products" ,tags=["products"])
+db_router = APIRouter(prefix="/db", tags=["products"])
+
 @router.get("/")
 async def  get_all_products():
     products = await all_products()
     if not products:
         raise HTTPException(status_code=404, detail="No products found ")
     return products
+
+
+@db_router.get("/products")
+async def get_database_products():
+    # Returns the list of products preloaded with Amazon reviews.
+    return await all_products()
+
+
+@db_router.get("/product/{id}")
+async def get_database_product_by_id(id: int):
+    # Returns a specific product from the reviews database.
+    prod = await get_product(id)
+    if not prod:
+        raise HTTPException(
+            status_code=404, detail="Product not found in reviews database"
+        )
+    return prod
     
